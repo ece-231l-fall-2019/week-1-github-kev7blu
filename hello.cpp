d21c333e56afc1f8ef62caf057c5a01e5ab27c6c
Kevin Henry <kev7blu@gmail.com>
2019-08-26 12:23:57 -0600

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "#My story" 
+	       	<< "__My name Kevin Henry__ "
+	       	<< "##**I'm from Albuquerque, NM** "
+	       	<< "####Learn how to code in C++ and review some C. "
+	       	<< "##__*I hope to get my degree, get a great job*tada*, maybe start a family and life etc.*__ "
+	        << "![starwars battlefront - battle of scariff](https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiQyJHehqHkAhWGGTQIHWFxAD0QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DJ48gd0GZGWk&psig=AOvVaw1QKkUvkGPXYtNY6Bk8WFrG&ust=1566926630994609) "
+		<< "(https://www.instagram.com/kev7blu/) "
+		<< std::endl;
+
+
+		// who you are, where you came from,
+		// what you are hoping to get out of this class,
+		// where you want to go in life
+		// picture
+		// instagram link
+
+	return 0;
+}
