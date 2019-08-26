abacdbddf0b6ab6f7f7fa0e3c6d50e89cdcb1ee0
Kevin Henry <kev7blu@gmail.com>
2019-08-26 12:41:47 -0600

    2nd time try

--- hello.cpp
+++ hello.cpp
@@ -2,14 +2,14 @@
 
 int main()
 {
-	std::cout << "#My story" 
-	       	<< "__My name Kevin Henry__ "
-	       	<< "##**I'm from Albuquerque, NM** "
-	       	<< "####Learn how to code in C++ and review some C. "
-	       	<< "##__*I hope to get my degree, get a great job*tada*, maybe start a family and life etc.*__ "
-	        << "![starwars battlefront - battle of scariff](https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiQyJHehqHkAhWGGTQIHWFxAD0QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DJ48gd0GZGWk&psig=AOvVaw1QKkUvkGPXYtNY6Bk8WFrG&ust=1566926630994609) "
-		<< "(https://www.instagram.com/kev7blu/) "
-		<< std::endl;
+	std::cout << "#My story " << std::endl;
+	std::cout << "__My name Kevin Henry__ " << std::endl;
+	std::cout << "##**I'm from Albuquerque, NM** " << std::endl;
+	std::cout << "####Learn how to code in C++ and review some C. " << std::endl;
+	std::cout << "##__*I hope to get my degree, get a great job:tada:, maybe start a family and life etc.*__ "            <<std::endl;
+	std::cout << "              " << std::endl;
+	std::cout << "![Starwars Battlefront-Battle of Scariff](https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiQyJHehqHkAhWGGTQIHWFxAD0QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DJ48gd0GZGWk&psig=AOvVaw1QKkUvkGPXYtNY6Bk8WFrG&ust=1566926630994609) " << std::endl;
+	std::cout << "(https://www.instagram.com/kev7blu/) "<< std::endl;
 
 
 		// who you are, where you came from,
