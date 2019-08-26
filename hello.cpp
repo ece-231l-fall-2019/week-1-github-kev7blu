4fbc91cd782e7ce9d953e1c6e04bd450061d99f7
Kevin Henry <kev7blu@gmail.com>
2019-08-26 12:53:14 -0600

    3rd try

--- hello.cpp
+++ hello.cpp
@@ -8,7 +8,7 @@ int main()
 	std::cout << "####Learn how to code in C++ and review some C. " << std::endl;
 	std::cout << "##__*I hope to get my degree, get a great job:tada:, maybe start a family and life etc.*__ "            <<std::endl;
 	std::cout << "              " << std::endl;
-	std::cout << "![Starwars Battlefront-Battle of Scariff](https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiQyJHehqHkAhWGGTQIHWFxAD0QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DJ48gd0GZGWk&psig=AOvVaw1QKkUvkGPXYtNY6Bk8WFrG&ust=1566926630994609) " << std::endl;
+	std::cout << "![Fisherman's Wharf-San Francisco](https://photos.google.com/photo/AF1QipP6hzSaZl0ZFyHsI6QhNEcv7d782XRA_9IXBv5H) " << std::endl;
 	std::cout << "(https://www.instagram.com/kev7blu/) "<< std::endl;
 
 
--- hello.md
+++ hello.md
@@ -4,5 +4,5 @@ __My name Kevin Henry__
 ####Learn how to code in C++ and review some C. 
 ##__*I hope to get my degree, get a great job:tada:, maybe start a family and life etc.*__ 
               
-![Starwars Battlefront-Battle of Scariff](https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiQyJHehqHkAhWGGTQIHWFxAD0QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DJ48gd0GZGWk&psig=AOvVaw1QKkUvkGPXYtNY6Bk8WFrG&ust=1566926630994609) 
+![Fisherman's Wharf-San Francisco](https://photos.google.com/photo/AF1QipP6hzSaZl0ZFyHsI6QhNEcv7d782XRA_9IXBv5H) 
 (https://www.instagram.com/kev7blu/) 
